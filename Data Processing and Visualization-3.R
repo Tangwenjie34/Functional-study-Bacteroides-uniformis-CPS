2d337f1a6efb705d0c2e1a38e965cbf1587b3330
Tangwenjie34 <83932955+Tangwenjie34@users.noreply.github.com>
2025-12-23 20:05:58 +0800

    Update Data Processing and Visualization-3.R

--- Data Processing and Visualization-3.R
+++ Data Processing and Visualization-3.R
@@ -15,24 +15,15 @@ library(reshape2)
 library(ggprism)
 library(ggbeeswarm)
 library(patchwork)
-c <- read.csv("./BU-rcsCCAPSULETHICHNESS.csv", row.names=1)
-group_names <- c( "WT","rcsC","C-rcsC")
-c$group <- factor( c$group, level=group_names )
-ggplot(c,aes(group,capsule,fill=group))+
-  geom_beeswarm(cex = 2.5, show.legend = FALSE,pch=21,size=3)+
-  stat_summary(fun=mean,fun.ymin=mean,fun.ymax=mean,geom='crossbar',width=0.3,size=0.5,color='black')+
-  stat_summary(geom = "errorbar",fun.data = 'mean_se', width = 0.2)+
-  scale_fill_manual(values = c("grey","#5cc3e8","#e95f5c"))+theme_classic()+
-  scale_y_continuous(limits = c(0,200))+
-  stat_compare_means(comparisons = list(c("WT","rcsC"), c("WT","C-rcsC"), c("C-rcsC","rcsC")) ,
-                     method = "wilcox.test",label = "p.signif")
+
+
 
 m3h.g2 <-read.delim("./ODVALUE.txt",header = T,row.names= 1)
 m3h.g2 %>% rowwise() %>% 
   mutate(mean_value=mean(c_across(rep1:rep5)),std_value=sd(c_across(rep1:rep5))) %>% 
   filter(Weeks>=1)-> new.dat
 colnames(new.dat)[8]<-"OD600"
-group_names <- c( "WT","rcsC","CrcsC")
+group_names <- c( "WT","rcsC")
 new.dat$group <- factor(new.dat$group, level=group_names )
 ggplot(new.dat,aes(x=Weeks,y=OD600))+
   geom_point(aes(color=group),size=0.91)+
@@ -41,7 +32,7 @@ ggplot(new.dat,aes(x=Weeks,y=OD600))+
                     ymax=OD600+std_value, color=group),width=0.42,size=0.45)+
   scale_y_continuous(limits = c(0,1))+
   scale_x_continuous(limits = c(0,72), breaks = seq(0,72,12))+
-  scale_color_manual(values = c("WT"="grey","rcsC"="#5cc3e8","CrcsC"="#e95f5c"))+
+  scale_color_manual(values = c("WT"="grey","rcsC"="#5cc3e8")+
   theme_classic()
 
 m3h.g1 <-read.delim("./ODVALUE.txt",header = T,row.names= 1)
@@ -51,7 +42,7 @@ m3h.g1 %>% rowwise() %>%
 colnames(new.dat)[9]<-"OD600"
 group_names <- c("Pectin", "Glucose","HG","HG-oligos","RG-I","RG-I-oligos")
 new.dat$group <- factor(new.dat$group, level=group_names )
-group_names <- c( "WT","rcsC","CrcsC")
+group_names <- c( "WT","rcsC")
 new.dat$strain <- factor(new.dat$strain, level=group_names )
 ggplot(new.dat,aes(x=Weeks,y=OD600))+
   geom_point(aes(color=strain),size=0.51)+
@@ -65,14 +56,14 @@ ggplot(new.dat,aes(x=Weeks,y=OD600))+
   scale_y_continuous(limits = c(0,1))+
   scale_x_continuous(limits = c(0,72),
                      breaks = seq(0,72,12))+
-  scale_color_manual(values = c("WT"="grey","rcsC"="#5cc3e8","CrcsC"="#e95f5c"))
+  scale_color_manual(values = c("WT"="grey","rcsC"="#5cc3e8"))
 
 c <- read.csv("./IgA-Elisa.csv", row.names=1)
-group_names <- c( "Con","WT", "rcsC",  "CrcsC" )
+group_names <- c( "Con","WT", "rcsC" )
 c$Group <- factor(c$Group, level=group_names )
 
 topbar <- function(x){
-  return(mean(x)+sd(x)/sqrt(length(x))) #误差采用了mean+-sem
+  return(mean(x)+sd(x)/sqrt(length(x))) #mean+-sem
 }
 bottombar <- function(x){
   return(mean(x)-sd(x)/sqrt(length(x)))
@@ -87,8 +78,43 @@ ggplot(c,aes(week,IgA,color=Group))+
                size=2.5,pch=21,color='black')+
   scale_y_continuous(limits = c(25, 250), breaks = seq(0, 250, 50))+
   scale_x_continuous(limits = c(5, 25), breaks = seq(5, 25, 5)) +
-  scale_color_manual(values = c("#ffdb00","grey", "#5cc3e8", "#e95f5c"))+
-  scale_fill_manual(values = c("#ffdb00","grey", "#5cc3e8", "#e95f5c"))+ 
-  theme() +
-  annotate(geom = 'text',label='***',x=22.5,y=134.2,size=4,angle=90)+
-  annotate(geom = 'text',label='****',x=23.8,y=125.12,size=4,angle=90)
+  scale_color_manual(values = c("#ffdb00","grey", "#5cc3e8"))+
+  scale_fill_manual(values = c("#ffdb00","grey", "#5cc3e8"))+ 
+  theme() 
+                     
+ a <- read.csv("./Iga-coating.csv", row.names=1)
+group_names <- c( "WT","rcsC")
+a$group <- factor( a$group, level=group_names )
+ggplot(a,aes(group,IgA_coating))+ 
+  geom_bar(colour="black",stat="summary",fun=mean,position=position_dodge(0.6),width = 0.65,fill='white')+
+  geom_jitter(aes(fill=group),shape=21,stroke=0.1,alpha=1,size=2,
+              width = 0.1, height = 0)+
+  stat_summary(geom = "errorbar",fun.data = 'mean_se', width = 0.35)+
+  scale_fill_manual(values = c(
+    "grey","#5cc3e8"))+
+  theme_classic()+
+  scale_y_continuous(limits = c(0, 22), breaks = seq(0,20, 5))
+
+a <- read.csv("./Iga-coating_1.csv", row.names=1)
+group_names <- c("HP-WT","HP-rcsC" , "LP-WT", "LP-rcsC")
+a$group <- factor( a$group, level=group_names )
+ggplot(a,aes(group,IgA_coating))+ 
+  geom_bar(colour="black",stat="summary",fun=mean,position=position_dodge(0.6),width = 0.65,fill='white')+
+  geom_jitter(aes(fill=group),shape=21,stroke=0.1,alpha=1,size=1.5,
+              width = 0.1, height = 0)+
+  stat_summary(geom = "errorbar",fun.data = 'mean_se', width = 0.35)+
+  scale_fill_manual(values = c("grey","#5cc3e8","grey","#5cc3e8"))+
+  theme_classic()+
+  scale_y_continuous(limits = c(0,40), breaks = seq(0,25, 5))
+
+a <- read.csv("IgA-LPMC.csv", row.names=1)
+group_names <- c("LPMC-SPF-WT","LPMC-SPF-rcsC" , "LPMC-GF-WT", "LPMC-GF-rcsC")
+a$group <- factor( a$group, level=group_names )
+ggplot(a,aes(group,IgA_coating))+ 
+  geom_bar(colour="black",stat="summary",fun=mean,position=position_dodge(0.6),width = 0.65,fill='white')+
+  geom_jitter(aes(fill=group),shape=21,stroke=0.1,alpha=1,size=1.5,
+              width = 0.1, height = 0)+
+  stat_summary(geom = "errorbar",fun.data = 'mean_se', width = 0.35)+
+  scale_fill_manual(values = c("grey","#5cc3e8","grey","#5cc3e8"))+
+  theme_classic()+
+  scale_y_continuous(limits = c(0,15), breaks = seq(0,15, 3))                   
